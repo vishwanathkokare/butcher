9e6c2bde5c0069576434ee8e9b54889dc46749e7
Vishwanath kokare <61642625+vishwanathkokare@users.noreply.github.com>
2025-01-15 12:02:44 +0000

    refactor

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,20 +1,21 @@
-import HomeBox from "./components/ui/HomeBox"
-import Chicken from "@/assets/chicken.webp"
-import Mutton from "@/assets/goat.jpg"
-import Eggs from '@/assets/eggs.webp'
+import HomeBox from "./components/ui/HomeBox";
+import Chicken from "@/assets/chicken.webp";
+import Mutton from "@/assets/goat.jpg";
+import Eggs from "@/assets/eggs.webp";
 const Home = () => {
-  return(   <div className="max-w-7xl mx-auto p-6 h-[90vh]">
-        <h1 className="text-4xl font-semibold text-left text-gray-800 mb-8">Categories</h1>
-        <div className="grid grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+  return (
+    <div className="max-w-7xl mx-auto p-6 h-[90vh]">
+      <h1 className="text-4xl font-semibold text-left text-gray-800 mb-8">
+        Categories
+      </h1>
+      <div className="grid grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <HomeBox Image={Chicken} Name="Chicken" />
 
         <HomeBox Image={Mutton} Name="Mutton" />
-            <HomeBox Image={Eggs} Name="Eggs" />
-           
-
-        </div>
+        <HomeBox Image={Eggs} Name="Eggs" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
