43417a4e9060b79455c7bc35f66ea01810de2484
Vishwanath kokare <61642625+vishwanathkokare@users.noreply.github.com>
2025-02-12 17:51:18 +0000

    ui ux better button with logo and theme

--- client/src/components/ui/ThemeToggle.tsx
+++ client/src/components/ui/ThemeToggle.tsx
@@ -1,14 +1,20 @@
 // filepath: /workspaces/butcher/client/src/components/ui/ThemeToggle.tsx
-import React from 'react';
+import React, { useState } from 'react';
+import { faSun,faMoon } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 const ThemeToggle: React.FC = () => {
+  const [theme,setTheme] = useState<Boolean>(false); 
   const toggleTheme = () => {
     if (document.documentElement.classList.contains('dark')) {
       document.documentElement.classList.remove('dark');
       localStorage.setItem('theme', 'light');
+      setTheme(true);
     } else {
       document.documentElement.classList.add('dark');
       localStorage.setItem('theme', 'dark');
+      setTheme(false);
     }
   };
 
@@ -16,14 +22,17 @@ const ThemeToggle: React.FC = () => {
     const theme = localStorage.getItem('theme');
     if (theme === 'dark') {
       document.documentElement.classList.add('dark');
+      setTheme(false);
     } else {
       document.documentElement.classList.remove('dark');
+      setTheme(true);
     }
   }, []);
 
   return (
-    <button onClick={toggleTheme} className="p-2 rounded bg-zinc-900 dark:bg-gray-200 text-white dark:text-black">
-      Toggle Theme
+    <button onClick={toggleTheme} className="p-2 px-4 rounded-full dark:bg-zinc-900 bg-white">
+      {theme && theme ? <FontAwesomeIcon icon={faMoon} className="text-zinc-800" /> :
+      <FontAwesomeIcon icon={faSun} className="text-yellow-200" />}
     </button>
   );
 };
