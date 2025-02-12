15944f899711a873b68d7f468af1596b2bb65807
Vishwanath kokare <61642625+vishwanathkokare@users.noreply.github.com>
2025-02-12 17:51:38 +0000

    ui ux change

--- client/src/components/ui/HomeBox.tsx
+++ client/src/components/ui/HomeBox.tsx
@@ -10,7 +10,8 @@ const HomeBox: React.FC<HomeBoxProps> = ({ Image, Name }) => {
   const url = `/products/${Name}?image=${encodeURIComponent(Image)}`;
 
   return (
-    <Link to={url} className="block cursor-pointer bg-white h-48 rounded-lg shadow-lg overflow-hidden">
+    <Link to={url} className="block cursor-pointer bg-white h-48 rounded-xl hover:scale-105 transition-transform
+ shadow-lg overflow-hidden">
       <img src={Image} alt={Name} className="w-full h-3/4 object-contain"/>
       <div className="p-4 text-right">
         <h2 className="text-xl font-semibold text-gray-700">{Name}</h2>
