63a5b4679f00f57731c2eed9aeba073db9da391e
Vishwanath kokare <61642625+vishwanathkokare@users.noreply.github.com>
2025-02-12 17:49:10 +0000

    modal close fix and theme implemention

--- client/src/components/ui/Modal.tsx
+++ client/src/components/ui/Modal.tsx
@@ -11,8 +11,8 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
 
   return (
     <div className="text-black fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white w-4/5 p-6 rounded-lg shadow-lg relative">
-        <button onClick={onClose} className="absolute top-2 right-2 text-gray-600 text-xl">
+      <div className="bg-gray-100 dark:bg-zinc-900 text-black dark:text-white w-4/5 p-6 rounded shadow-lg relative">
+        <button onClick={onClose} className="absolute top-2 right-2 text-gray-600 text-5xl">
           &times;
         </button>
         {children}
