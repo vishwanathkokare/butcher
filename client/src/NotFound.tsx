24730e5d4679dd10cad78b340711dece28c308e5
Vishwanath kokare <61642625+vishwanathkokare@users.noreply.github.com>
2025-02-12 17:56:11 +0000

    theme fix of text color

--- client/src/NotFound.tsx
+++ client/src/NotFound.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NotFoundPage: React.FC = () => {
   return (
-    <div className='bg-gray-200 dark:bg-zinc-900 h-screen flex flex-col items-center justify-center'>
+    <div className='bg-gray-200 dark:bg-zinc-800 h-screen flex flex-col items-center justify-center text-black dark:text-white'>
       <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
       <Link to="/">Go back to Home</Link>
