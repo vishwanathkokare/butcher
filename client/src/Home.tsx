55e12aaaffe084e7a67df3982ac868d20ac3f3c2
Vishwanath kokare <61642625+vishwanathkokare@users.noreply.github.com>
2025-02-12 17:53:01 +0000

    images fix

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,11 +1,11 @@
 import HomeBox from "./components/ui/HomeBox";
 import Chicken from "@/assets/chicken.webp";
-import Mutton from "@/assets/goat.jpg";
-import Eggs from "@/assets/eggs.webp";
+import Mutton from "@/assets/goat.jpeg";
+import Eggs from "@/assets/egg.jpg";
 import Beef from "@/assets/beef.png";
 const Home = () => {
   return (
-    <div className="bg-gray-200 dark:bg-zinc-900 h-screen">
+    <div className="bg-gray-200 dark:bg-zinc-800 h-screen">
     <div className=" container mx-auto p-6 pb-24">
       <div className="grid grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <HomeBox Image={Chicken} Name="Chicken" />
