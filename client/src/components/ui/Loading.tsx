14a501fbd5807618de1793313ad55802c05efc97
Vishwanath kokare <61642625+vishwanathkokare@users.noreply.github.com>
2025-02-12 17:48:30 +0000

    loading bug fix of theme mode and spinning

--- client/src/components/ui/Loading.tsx
+++ client/src/components/ui/Loading.tsx
@@ -1,10 +1,12 @@
 const Loading = () => {
   return (
-    <div className="bg-gray-200 dark:bg-zinc-900 flex items-center justify-center h-screen">
+    <div className="bg-gray-200 dark:bg-zinc-800 flex items-center justify-center h-screen">
       <div
-        className="spinner-border animate-spin inline-block w-14 h-14 border-8 rounded-full"
+        className="animate-spin inline-block w-14 h-14 border-8 border-t-transparent border-gray-500 rounded-full"
         role="status"
-      ></div>
+      >
+        <span className="sr-only">Loading...</span>
+      </div>
     </div>
   );
 };
