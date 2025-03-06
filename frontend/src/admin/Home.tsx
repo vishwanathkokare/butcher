041216c74198b824778a619e989786d398563cd0
Vishwanath kokare <61642625+vishwanathkokare@users.noreply.github.com>
2025-03-06 22:21:07 +0000

    same bg

--- frontend/src/admin/Home.tsx
+++ frontend/src/admin/Home.tsx
@@ -59,7 +59,7 @@ const Home: React.FC = () => {
   return (
     <div className="bg-gray-200 dark:bg-zinc-800 mx-auto py-10 px-12 dark:text-white">
       <h1 className="text-2xl font-bold mb-4">Orders</h1>
-      <Table>
+      <Table className="dark:border-black">
         <TableHeader>
           <TableRow>
             <TableHead>Name</TableHead>
